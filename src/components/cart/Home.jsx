648e95a246feb3f1617edb83bfd9047374646eab
Sharvanth-05 <cherrygamer121203@gmail.com>
2025-11-06 23:08:34 +0530

    Fixed the allItems not found error at cart dir, commented it out.

--- src/components/cart/Home.jsx
+++ src/components/cart/Home.jsx
@@ -8,11 +8,11 @@ export default function MarketPage() {
   //search logic
   const [search, setSearch] = useState("");
 
-  const filteredItems = search
-    ? allItems.filter((item) =>
-        item.name.toLowerCase().includes(search.toLowerCase())
-      )
-    : allItems;
+  // const filteredItems = search
+  //   ? allItems.filter((item) =>
+  //       item.name.toLowerCase().includes(search.toLowerCase())
+  //     )
+  //   : allItems;
   return (
     <div className="flex flex-col w-full mt-5">
       <CartHeader search={search} setSearch={setSearch} />
