bf892f7a3ef3c0cb4dfec7e9e104c1121925f264
Sharvanth-05 <cherrygamer121203@gmail.com>
2025-11-06 23:15:14 +0530

    Commented out the entire cart dir for avoiding errors while deploying

--- src/components/cart/CartHeader.jsx
+++ src/components/cart/CartHeader.jsx
@@ -1,14 +1,14 @@
-import { LiaShoppingBagSolid as BagOutline } from "react-icons/lia";
-import PageHeader from "@/components/layout/PageHeader";
+// import { LiaShoppingBagSolid as BagOutline } from "react-icons/lia";
+// import PageHeader from "@/components/layout/PageHeader";
 
-export default function CartHeader({search,setSearch}) {
-  return (
-    <PageHeader
-      search={search}
-      setSearch={setSearch}
-      title="Bag"
-      placeholder="Search in bag.."
-      icon={<BagOutline />}
-    />
-  );
-}
+// export default function CartHeader({search,setSearch}) {
+//   return (
+//     <PageHeader
+//       search={search}
+//       setSearch={setSearch}
+//       title="Bag"
+//       placeholder="Search in bag.."
+//       icon={<BagOutline />}
+//     />
+//   );
+// }
--- src/components/cart/Home.jsx
+++ src/components/cart/Home.jsx
@@ -1,23 +1,23 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import CartGrid from "./CartGrid";
-import CartHeader from "./CartHeader";
+// import { useState } from "react";
+// import CartGrid from "./CartGrid";
+// import CartHeader from "./CartHeader";
 
-export default function MarketPage() {
-  //search logic
-  const [search, setSearch] = useState("");
+// export default function MarketPage() {
+//   //search logic
+//   const [search, setSearch] = useState("");
 
-  // const filteredItems = search
-  //   ? allItems.filter((item) =>
-  //       item.name.toLowerCase().includes(search.toLowerCase())
-  //     )
-  //   : allItems;
-  return (
-    <div className="flex flex-col w-full mt-5">
-      <CartHeader search={search} setSearch={setSearch} />
-      <CartGrid items={filteredItems} />
-    </div>
-  );
+//   // const filteredItems = search
+//   //   ? allItems.filter((item) =>
+//   //       item.name.toLowerCase().includes(search.toLowerCase())
+//   //     )
+//   //   : allItems;
+//   return (
+//     <div className="flex flex-col w-full mt-5">
+//       <CartHeader search={search} setSearch={setSearch} />
+//       <CartGrid items={filteredItems} />
+//     </div>
+//   );
   
-}
+// }
